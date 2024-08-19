0be702bcbb1e60847aea42a5cc19ef5bfbfd411c
Tarek Omar Pacha <t.omar_pacha@me.com>
2024-08-19 22:39:37 +0200

    nue files

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "azure_rg_name" {
+  type        = string
+  description = "The Azure RG is Name"
+  default     = "TOP-prod-rg"
+}
+
+variable "azure_location" {
+  type        = string
+  description = "The Azure Region"
+  default     = "West Europe"
+
+}
